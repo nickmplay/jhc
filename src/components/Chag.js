753705f955595c19befe8964842a19a27355f436
Nick M <nickmontagni@hotmail.com>
2017-09-24 05:16:40 +0100

    added options toggler

---
+++ src/components/Chag.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+export default class Chag extends React.Component {
+  constructor(props){
+    super(props);
+    this.processButton = this.processButton.bind(this);
+  }
+
+  processButton = () => {
+    this.props.toggleChag(this.props.bName);
+  }
+
+  render(){
+    let cn;
+    if( this.props.selected ) {
+      cn = "button-chosen";
+    } else {
+      cn = "button-not-chosen";
+    }
+    return (
+        <div>
+        <button
+          onClick={this.processButton}
+          className={cn}  
+          name={this.props.bName}
+        >
+        {this.props.name}
+        </button>    
+        </div>
+    );
+  }
+}
--- src/components/NewApp.js
+++ src/components/NewApp.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import Chag from './Chag';
+
 
 export default class NewApp extends React.Component {
-  
+  constructor(props){
+    super(props);
+    this.toggleChag = this.toggleChag.bind(this);
+  }
+  state = {
+    selected : ["p1","p3"]
+  };
+
+  toggleChag = (Chag) => {
+    let bPresent = (this.state.selected.indexOf(Chag) > -1);
+    if (bPresent){
+      this.setState((prevState) => ({
+        selected: prevState.selected.filter((e) => e !== Chag)
+      }));
+    } else {
+      this.setState((prevState) => ({
+        selected: prevState.selected.concat(Chag)
+      }));
+    }
+  }
 
   render() {
     return (
       <div>
-        <h1>Boiler Plate</h1>
+        <h1>Count Jewish Holidays</h1>
+        <h4>Select chagim</h4>
+        <Chag toggleChag={this.toggleChag} name="Pesach 1" bName="p1" selected={this.state.selected.indexOf("p1") > -1}/>
+        <Chag toggleChag={this.toggleChag} name="Pesach 2" bName="p2" selected={this.state.selected.indexOf("p2") > -1}/>
+        <Chag toggleChag={this.toggleChag} name="Pesach 3" bName="p3" selected={this.state.selected.indexOf("p3") > -1}/>
 
       </div>
     );
