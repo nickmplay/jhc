866861c997919aa9e9ae18a20ee3065724b4b8d4
Nick M <nickmontagni@hotmail.com>
2017-09-24 05:12:00 +0100

    Boiler plate

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NewApp from './components/NewApp';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<NewApp />, document.getElementById('app'));
---
+++ src/components/NewApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class NewApp extends React.Component {
+  
+
+  render() {
+    return (
+      <div>
+        <h1>Boiler Plate</h1>
+
+      </div>
+    );
+  }
+}
