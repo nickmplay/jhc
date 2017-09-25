b160dd22461563728be37e987b450064b6305c47
Nick M <nickmontagni@hotmail.com>
2017-09-25 21:08:53 +0100

    Added date pickers

--- src/components/NewApp.js
+++ src/components/NewApp.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
 import Chag from './Chag';
 import Calc from './Calc';
 import ResultsTable from './ResultsTable';
@@ -15,11 +18,19 @@ export default class NewApp extends React.Component {
     super(props);
     this.toggleChag = this.toggleChag.bind(this);
     this.countHolidays = this.countHolidays.bind(this);
+    this.onStartDateChange = this.onStartDateChange.bind(this);
+    this.onStartDateFocusChange = this.onStartDateFocusChange.bind(this);
+    this.onEndDateChange = this.onEndDateChange.bind(this);
+    this.onEndDateFocusChange = this.onEndDateFocusChange.bind(this);
   }
   state = {
     selected : ["p1","p3"],
     displayTable : false,
-    tableData: dummyResults
+    tableData: dummyResults,
+    startDate: moment("2017-01-01"),
+    startDateFocused: false,
+    endDate: moment("2017-12-31"),
+    endDateFocused: false
   };
 
   toggleChag = (Chag) => {
@@ -41,11 +52,50 @@ export default class NewApp extends React.Component {
     }));
   }
 
+  onStartDateChange = (startDate) => {
+    this.setState(() => ({ startDate }));
+  }
+
+  onStartDateFocusChange = ({ focused }) => {
+    this.setState(() => ({ startDateFocused: focused }));
+  }
+
+  onEndDateChange = (endDate) => {
+    this.setState(() => ({ endDate }));
+  }
+
+  onEndDateFocusChange = ({ focused }) => {
+    this.setState(() => ({ endDateFocused: focused }));
+  }
+
   render() {
     return (
       <div>
         <h1>Count Jewish Holidays</h1>
-        <h4>Select chagim</h4>
+        <h4>Enter the start and end dates, select chagim, then push Calculate to see how many days of annual leave you'll need </h4>
+
+        <p>Start Date: </p>
+        <SingleDatePicker
+          date={this.state.startDate}
+          onDateChange={this.onStartDateChange}
+          displayFormat="DD/MM/YYYY"
+          focused={this.state.startDateFocused}
+          onFocusChange={this.onStartDateFocusChange}
+          numberOfMonths={1}
+          isOutsideRange={() => false}
+        />
+
+        <p>End Date: </p>
+        <SingleDatePicker
+          date={this.state.endDate}
+          onDateChange={this.onEndDateChange}
+          displayFormat="DD/MM/YYYY"
+          focused={this.state.endDateFocused}
+          onFocusChange={this.onEndDateFocusChange}
+          numberOfMonths={1}
+          isOutsideRange={() => false}
+        />
+
         <Chag toggleChag={this.toggleChag} name="Pesach 1" bName="p1" selected={this.state.selected.indexOf("p1") > -1}/>
         <Chag toggleChag={this.toggleChag} name="Pesach 2" bName="p2" selected={this.state.selected.indexOf("p2") > -1}/>
         <Chag toggleChag={this.toggleChag} name="Pesach 3" bName="p3" selected={this.state.selected.indexOf("p3") > -1}/>
