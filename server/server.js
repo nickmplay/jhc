d7f93b5e856ab0d012d7dffbb1587b75a52891a2
Nick M <nickmontagni@hotmail.com>
2017-09-29 10:04:36 +0100

    Configured query API request

--- server/server.js
+++ server/server.js
@@ -5,6 +5,17 @@ const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(express.static(publicPath));
 
+//query 'database' API
+app.get('/:startDate/:endDate/:chagStr', (req, res) => {
+  console.log(req.params.startDate, 'to', req.params.endDate, 'for', req.params.chagStr );
+  res.send(req.params.startDate + ' to ' + req.params.endDate + ' for ' + req.params.chagStr);
+});
+
+//bad request
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
 app.listen(3000, () => {
   console.log('server is up on port 3000');
 });
